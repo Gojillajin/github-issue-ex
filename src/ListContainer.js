9e6d30b0900247657d7c813020920e7510fb97f3
김진영 <gojillajin@gmail.com>
2022-11-27 22:50:05 +0900

    221127-1

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
 import Button from "./components/Button";
 import styles from "./ListContainer.module.css";
 
 export default function ListContainer() {
+  const [inputValue, setInputValue] = useState("is:pr is:open");
   return (
     <div className={styles.listContainer}>
-      <Button
-        style={{
-          fontSize: "14px",
-          backgroundColor: "green",
-          color: "white",
-        }}
-      >
-        New Issue
-      </Button>
+      <div className={styles.topSection}>
+        <input
+          className={styles.input}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "green",
+            color: "white",
+          }}
+        >
+          New Issue
+        </Button>
+      </div>
     </div>
   );
 }
