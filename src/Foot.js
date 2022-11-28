7f4e28d1c85dec02e15cbcb9de8f2ba1ecbf0da5
김진영 <gojillajin@gmail.com>
2022-11-29 00:07:38 +0900

    221129 footer

--- src/Foot.js
+++ src/Foot.js
@@ -1,5 +1,26 @@
 import styles from "./Foot.module.css";
 
+const footerItems = [
+  {
+    title: "Terms",
+    link: `https://github-terms/github-terms-of-service`,
+  },
+  {
+    title: "Privacy",
+    link: `https://github-terms/github-terms-of-service`,
+  },
+];
+
 export default function Foot() {
-  return <div className={styles.footer}>footer</div>;
+  return (
+    <ul className={styles.footer}>
+      {footerItems.map(({ link, title }) => (
+        <li className={styles.item} key={title}>
+          <a className={styles.link} href={link}>
+            {title}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 }
